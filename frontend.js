4c3dd78a2b997d855fbfa76db7feb4308e09b3a1
roooot <103732419+rooootdev@users.noreply.github.com>
2024-09-25 09:46:48 +0200

    Update frontend.js

--- frontend.js
+++ frontend.js
@@ -1,18 +1,18 @@
 async function initServiceWorker() {
-    let swRegistration = await navigator.serviceWorker.register('https://andreinwald.github.io/webpush-ios-example/serviceworker.js', {scope: '/webpush-ios-example/'})
+    let swRegistration = await navigator.serviceWorker.register('./serviceworker.js', { scope: './' });
     let pushManager = swRegistration.pushManager;
 
     if (!isPushManagerActive(pushManager)) {
         return;
     }
 
-    let permissionState = await pushManager.permissionState({userVisibleOnly: true});
+    let permissionState = await pushManager.permissionState({ userVisibleOnly: true });
     switch (permissionState) {
         case 'prompt':
             document.getElementById('subscribe_btn').style.display = 'block';
             break;
         case 'granted':
-            displaySubscriptionInfo(await pushManager.getSubscription())
+            displaySubscriptionInfo(await pushManager.getSubscription());
             break;
         case 'denied':
             document.getElementById('subscribe_btn').style.display = 'none';
@@ -36,8 +36,6 @@ function isPushManagerActive(pushManager) {
 }
 
 async function subscribeToPush() {
-    // Public part of VAPID key, generation of that covered in README
-    // All subscription tokens associated with that key, so if you change it - you may lose old subscribers
     const VAPID_PUBLIC_KEY = 'BAwUJxIa7mJZMqu78Tfy2Sb1BWnYiAatFCe1cxpnM-hxNtXjAwaNKz1QKLU8IYYhjUASOFzSvSnMgC00vfsU0IM';
 
     let swRegistration = await navigator.serviceWorker.getRegistration();
@@ -52,29 +50,26 @@ async function subscribeToPush() {
     try {
         let subscription = await pushManager.subscribe(subscriptionOptions);
         displaySubscriptionInfo(subscription);
-        // Here you can send fetch request with subscription data to your backend API for next push sends from there
     } catch (error) {
         document.getElementById('active_sub').style.display = 'block';
-        document.getElementById('active_sub').innerHTML = 'User denied push permission';
+        document.getElementById('active_sub').innerHTML = 'denied';
     }
 }
 
 function displaySubscriptionInfo(subscription) {
     document.getElementById('subscribe_btn').style.display = 'none';
     document.getElementById('active_sub').style.display = 'block';
-    document.getElementById('active_sub').innerHTML = '<b>Active subscription:</b><br><br>'
-        + JSON.stringify(subscription.toJSON());
-    document.getElementById('test_send_btn').style.display = 'block';
+    console.log(JSON.stringify(subscription.toJSON()));
 }
 
 function testSend() {
     const title = "Push title";
     const options = {
-        body: "Additional text with some description",
-        icon: "https://andreinwald.github.io/webpush-ios-example/images/push_icon.jpg",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg/1920px-Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg",
+        body: "smoerr_news_subscribed",
+        icon: "./images/push_icon.jpg",
+        image: "./images/push_image.jpg",
         data: {
-            "url": "https://andreinwald.github.io/webpush-ios-example/?page=success",
+            "url": "./?page=success",
             "message_id": "your_internal_unique_message_id_for_tracking"
         },
     };
@@ -84,7 +79,7 @@ function testSend() {
 }
 
 if ((new URLSearchParams(window.location.search)).get('page') === 'success') {
-    document.getElementById('content').innerHTML = 'You successfully opened page from WebPush! (this url was that was set in json data param)';
+    document.getElementById('content').innerHTML = 'Smoerr News wurden Abboniert';
 }
 
 if (navigator.serviceWorker) {
