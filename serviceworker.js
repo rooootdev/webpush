0c215a600c1aefb133f7946e2fa051bd59438114
roooot <103732419+rooootdev@users.noreply.github.com>
2024-09-25 09:49:23 +0200

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,13 +1,10 @@
 self.addEventListener('push', (event) => {
-    // PushData keys structure standart https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
     let pushData = event.data.json();
     if (!pushData || !pushData.title) {
         console.error('Received WebPush with an empty title. Received body: ', pushData);
     }
     self.registration.showNotification(pushData.title, pushData)
         .then(() => {
-            // You can save to your analytics fact that push was shown
-            // fetch('https://your_backend_server.com/track_show?message_id=' + pushData.data.message_id);
         });
 });
 
@@ -25,7 +22,5 @@ self.addEventListener('notificationclick', function (event) {
 
     clients.openWindow(event.notification.data.url)
         .then(() => {
-            // You can send fetch request to your analytics API fact that push was clicked
-            // fetch('https://your_backend_server.com/track_click?message_id=' + pushData.data.message_id);
         });
 });
